629eb48dcb6c6045208d30183ce6f2662ef36d8e
NikitaPankinn <angelospankin@yandex.ru>
2024-08-30 14:58:40 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,6 @@
 # Никита Панькин, 20-я когорта — Финальный проект.
 URL_SERVICE = "https://ef748b45-8c9f-4625-acd1-1a9dbe7a1a9f.serverhub.praktikum-services.ru"
-CREAT_ORDERS = "/api/v1/orders"
+# URL API — Создание заказа
+url_create = server_url +"/api/v1/orders"
+# URL API — Получение заказа по номеру трека
+url_get = server_url + "/api/v1/orders/track?t="
