c0765a325f553452db8661f1f4e162754dae125e
NikitaPankinn <angelospankin@yandex.ru>
2024-08-30 15:01:14 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,30 +1,19 @@
 # Никита Панькин, 20-я когорта — Финальный проект.
 
-import configuration
+# Импортируем библиотеку requests
 import requests
-import data
+# Импортируем URL API — Создание заказа и URL API — Получение заказа по номеру трека
+from configuration import url_create, url_get
 
-# Создание заказа
-def create_order(body):
-    return requests.post (configuration.URL_SERVICE + configuration.CREAT_ORDERS,
-                         json=body)
 
-# Получение заказа по номеру трекера
-def get_order(track_number):
-    get_order_url = f"{configuration.URL_SERVICE}/api/v1/orders/track?t={track_number}"
-    response = requests.get(get_order_url)
-    return response
+def create_order(order_data):
+    # Создание заказа
+    response_create = requests.post(url_create, json=order_data)
+    return response_create
 
-# Автотест
-def test_order_creation_and_retrieval():
-    response = create_order(data.order_body)
 
-    track_number = response.json()["track"]
-  
-
-# Получение данных заказа по треку
-    order_response = get_order(track_number)
-
-    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
-    order_data = order_response.json()
-
+def get_order(track):
+    # Получение информации о заказе по номеру трека
+    url = url_get + str(track)
+    response_get = requests.get(url)
+    return response_get
